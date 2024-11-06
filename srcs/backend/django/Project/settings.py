5413ec835676fe4a7ab2dd220473f571bb7bba4b
Samuel_Guillot <samuelguillot75@gmail.com>
2024-11-06 19:18:54 +0100

    tests in dockerfile of django non functionnal

--- srcs/backend/django/Project/settings.py
+++ srcs/backend/django/Project/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
---
+++ srcs/backend/django/tools/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+PINK='\033[1;35m'
+NC='\033[0m'
+
+echo -e "${YELLOW}Starting Django setup...${NC}"
+
+start_time=$(date +%s)
+end_time=$((start_time + 60))
+
+while ! nc -z postgresql 5432; do
+  echo -e "${BLUE}Waiting for PostgreSQL...${NC}"
+  sleep 2
+
+  if [ $(date +%s) -ge $end_time ]; then
+    echo -e "${RED}Failed to access the PostgreSQL container in 60 seconds.${NC}"
+    exit 1
+  fi
+done
+
+echo -e "${PINK}PostgreSQL is ready, starting Django...${NC}"
+
+cd /app
+exec python manage.py runserver 0.0.0.0:8000
