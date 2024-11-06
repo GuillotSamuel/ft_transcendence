dc7d172692b566c0ce893912464faf9ef7b3f584
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-06 16:11:25 +0100

    POSTRGRE

--- srcs/backend/django/project/Project/settings.py
+++ srcs/backend/django/project/Project/settings.py
@@ -72,14 +72,18 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'postgresql',  # Assurez-vous que ce nom correspond au service dans docker-compose.yml
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
