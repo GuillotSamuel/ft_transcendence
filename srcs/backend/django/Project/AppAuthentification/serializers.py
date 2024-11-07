db24353eee8c4edfb64a220ffb550709ad384092
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-08 00:09:01 +0100

    fix JWT

--- srcs/backend/django/Project/AppAuthentification/serializers.py
+++ srcs/backend/django/Project/AppAuthentification/serializers.py
@@ -1,16 +1,15 @@
-from django.contrib.auth.models import User  # Importe le modèle User de Django
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 class GameUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User  # Référence le modèle User de Django
+        model = get_user_model()  # Utilise le modèle d'utilisateur personnalisé
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        # Utilise create_user pour que le mot de passe soit haché
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
--- srcs/backend/django/tools/entrypoint.sh
+++ srcs/backend/django/tools/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Définit les couleurs pour l'affichage
 BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -7,21 +8,35 @@ RED='\033[0;31m'
 PINK='\033[1;35m'
 NC='\033[0m'
 
+# Affiche un message d'initialisation
 echo -e "${YELLOW}Starting Django setup...${NC}"
 
+# Enregistre le temps de démarrage et définit un timeout de 60 secondes
 start_time=$(date +%s)
 end_time=$((start_time + 60))
 
+# Attend que le service PostgreSQL soit disponible
 while ! nc -z postgresql 5432; do
   echo -e "${BLUE}Waiting for PostgreSQL...${NC}"
   sleep 2
 
+  # Si PostgreSQL n'est pas disponible après 60 secondes, échoue le script
   if [ $(date +%s) -ge $end_time ]; then
     echo -e "${RED}Failed to access the PostgreSQL container in 60 seconds.${NC}"
     exit 1
   fi
 done
 
+# Si PostgreSQL est prêt, continue le setup de Django
 echo -e "${PINK}PostgreSQL is ready, starting Django...${NC}"
 
+# Applique les migrations de Django (si elles existent)
+echo -e "${GREEN}Running makemigrations...${NC}"
+python /app/Project/manage.py makemigrations || true  # N'échoue pas si aucune migration n'est nécessaire
+
+echo -e "${GREEN}Running migrations...${NC}"
+python /app/Project/manage.py migrate
+
+# Lance le serveur Django
+echo -e "${GREEN}Starting Django server...${NC}"
 exec python /app/Project/manage.py runserver 0.0.0.0:8000
