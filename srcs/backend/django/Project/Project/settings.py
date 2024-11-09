154ac0b87d093c73e98aafdbe895074b5309d4d5
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-09 03:20:39 +0100

    fix settings

--- srcs/backend/django/Project/Project/settings.py
+++ srcs/backend/django/Project/Project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hb1d&ouy^o*z(tw*9z#yq#jy8e1h2z3kuo06oh$yos)zjl$##*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
