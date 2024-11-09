6fcb11a5d404c9a2da95cb931a630c3b4725aede
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-10 00:59:15 +0100

    cookies

--- srcs/backend/django/Project/AppAuthentification/checkAuthentification.py
+++ srcs/backend/django/Project/AppAuthentification/checkAuthentification.py
@@ -5,27 +5,18 @@ from rest_framework_simplejwt.tokens import RefreshToken
 class JWTCookieAuthentication(JWTAuthentication):
     def authenticate(self, request):
         access_token = request.COOKIES.get('access_token')
-        if not access_token:
-            return None
-
+        refresh_token = request.COOKIES.get('refresh_token')
         try:
             validated_token = self.get_validated_token(access_token)
             user = self.get_user(validated_token)
             return (user, validated_token)
         except InvalidToken:
-            refresh_token = request.COOKIES.get('refresh_token')
-            if not refresh_token:
-                return None
-
             try:
                 refresh = RefreshToken(refresh_token)
-                access_token = str(refresh.access_token)
-                request.COOKIES['access_token'] = access_token
-
-                validated_token = self.get_validated_token(access_token)
+                new_access_token = str(refresh.access_token)
+                request.COOKIES['access_token'] = new_access_token
+                validated_token = self.get_validated_token(new_access_token)
                 user = self.get_user(validated_token)
                 return (user, validated_token)
-            except TokenError:
+            except Exception:
                 return None
-
-        return None
