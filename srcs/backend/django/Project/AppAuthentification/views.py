e0e5bf0eebddb83b26d4e57865228d887e8e43da
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-09 11:50:00 +0100

    fix refreshToken

--- srcs/backend/django/Project/AppAuthentification/views.py
+++ srcs/backend/django/Project/AppAuthentification/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -14,6 +15,7 @@ def register(request):
         serializer.save()
         return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -31,26 +33,37 @@ def login(request):
     response.set_cookie(
         'access_token',
         str(accessToken),
-        max_age=3000,
+        max_age=20,
         httponly=True,
         samesite='Lax',
     )
     response.set_cookie(
         'refresh_token',
         str(refreshToken),
-        max_age=10000,
+        max_age=30,
         httponly=True,
         samesite='Lax',
     )
     return response
 
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def refreshToken(request):
+    refresh_token = request.COOKIES.get('refresh_token')
+    if not refresh_token:
+        return Response({'message': 'Refresh token missing'}, status=status.HTTP_400_BAD_REQUEST)
     try:
-        refresh_token = request.data.get('refresh_token')
-        refresh = RefreshToken(refresh_token)
-        new_access_token = str(refresh.access_token)
-        return Response({'access_token': new_access_token})
-    except Exception as e:
-        return Response({'error': 'Invalid or expired refresh token'}, status=status.HTTP_401_UNAUTHORIZED)
+        rtoken = RefreshToken(refresh_token)
+        access_token = rtoken.access_token
+        response = Response({'message': 'Token refreshed successfully'})
+        response.set_cookie(
+            'access_token',
+            str(access_token),
+            max_age=20,
+            httponly=True,
+            samesite='Lax',
+        )
+        return response
+    except TokenError:
+        return Response({'message': 'Invalid refresh token'}, status=status.HTTP_400_BAD_REQUEST)
