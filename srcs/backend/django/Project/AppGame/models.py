52d242883a77ca217b167a6d8cb179fbb6933f7a
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-14 02:18:36 +0100

    aaaaaa

--- srcs/backend/django/Project/AppGame/models.py
+++ srcs/backend/django/Project/AppGame/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from AppAuthentification.models import GameUser
 
-# Create your models here.
+class Player(models.Model):
+    PlayerName = models.CharField(max_length=150, unique=True, blank=False)
+    GameUser = models.ForeignKey(GameUser, on_delete=models.CASCADE)
+
+
+class Match(models.Model):
+    MatchName = models.CharField(max_length=150, unique=True, blank=False)
+    player1 = models.ForeignKey(Player, on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Player, on_delete=models.CASCADE)
+    player1_score = models.IntegerField(default=0)
+    player2_score = models.IntegerField(default=0)
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE)
