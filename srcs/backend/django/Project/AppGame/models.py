69e2b21cae15b5e211e9e7b364b7866287a736b2
Francois <transfo@Francoiss-MacBook-Air.local>
2024-11-14 02:32:31 +0100

    tttt

--- srcs/backend/django/Project/AppGame/models.py
+++ srcs/backend/django/Project/AppGame/models.py
@@ -8,8 +8,8 @@ class Player(models.Model):
 
 class Match(models.Model):
     MatchName = models.CharField(max_length=150, unique=True, blank=False)
-    player1 = models.ForeignKey(Player, on_delete=models.CASCADE)
-    player2 = models.ForeignKey(Player, on_delete=models.CASCADE)
+    player1 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='matches_player1')
+    player2 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='matches_player2')
     player1_score = models.IntegerField(default=0)
     player2_score = models.IntegerField(default=0)
-    winner = models.ForeignKey(Player, on_delete=models.CASCADE)
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='matches_winner')
